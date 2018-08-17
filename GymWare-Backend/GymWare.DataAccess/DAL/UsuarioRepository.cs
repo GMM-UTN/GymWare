0c4575fade2dfc690ce51c4557e29c163fae5bcb
Bautista Garaventa <bautistagaraventa@hotmail.com>
2018-08-16 23:27:16 -0300

    Login finished and working!

---
+++ GymWare-Backend/GymWare.DataAccess/DAL/UsuarioRepository.cs
@@ -0,0 +1,25 @@
+﻿using GymWare.Entities;
+using GymWare.Entities.DTO;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymWare.DataAccess.DAL
+{
+    public class UsuarioRepository : BaseRepository
+    {
+        public Cliente CheckCliente(UsuarioLoginDTO usuarioLoginDTO)
+        {
+            return _db.Clientes.FirstOrDefault(x => x.NombreUsuario == usuarioLoginDTO.Usuario && x.Contrasenia == usuarioLoginDTO.Contrasenia);
+        }
+
+        public Empleado CheckEmpleado(UsuarioLoginDTO usuarioLoginDTO)
+        {
+            return _db.Empleados.FirstOrDefault(x => x.NombreUsuario == usuarioLoginDTO.Usuario && x.Contrasenia == usuarioLoginDTO.Contrasenia);
+        }
+    }
+}
---
+++ GymWare-Backend/GymWare.Logic/UsuarioLogic.cs
@@ -0,0 +1,37 @@
+﻿using GymWare.DataAccess.DAL;
+using GymWare.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GymWare.Entities.DTO;
+
+namespace GymWare.Logic
+{
+    public class UsuarioLogic
+    {
+        private UsuarioRepository _us = new UsuarioRepository();
+        
+        public object CheckUsuario(UsuarioLoginDTO usuarioLoginDTO)
+        {
+            Cliente cliente = _us.CheckCliente(usuarioLoginDTO);
+            if (cliente != null)
+            {
+                return cliente;
+            }
+            else
+            {
+                Empleado empleado = _us.CheckEmpleado(usuarioLoginDTO);
+                if (empleado != null)
+                {
+                    return empleado;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
